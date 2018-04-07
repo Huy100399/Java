33f16e150ddda2c84184c483aee2a8f41090a62e
huy100399 <moclop1999@gmail.com>
2018-04-07 23:28:03 +0700

    commit

--- JavaCoBan/src/regular/expressions/RegularEpressions.java
+++ JavaCoBan/src/regular/expressions/RegularEpressions.java
@@ -1,9 +1,26 @@
 package regular.expressions;
 
+import java.util.Scanner;
+
 public class RegularEpressions {
 
+//	/http://laptrinhandroid.vn/bieu-thuc-chinh-quy-regular-expressions-trong-java/
+	//https://www.javatpoint.com/java-regex
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String regex= "";
+		boolean match=false;
+		while(match==false)
+		{
+			System.out.println("nhap vao ten cua ban :");
+			String s= new Scanner(System.in).nextLine();
+			System.out.println(s.matches(regex));
+			if(s.matches(regex)==true) break;
+			
+		}
+		
 
 	}
 
